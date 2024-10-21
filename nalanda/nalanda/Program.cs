220d8d3ad3671fd5b50406530fae9847f7149298
Amitesh1318 <sangrah007@gmail.com>
2024-10-21 20:02:42 +0530

    Initial-Commit

---
+++ nalanda/nalanda/Program.cs
@@ -0,0 +1,25 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+// // Add a default route for testing
+// app.MapGet("/", () => "Hello World!");  // This is the new route
+
+// Map the controllers (your existing API routes)
+app.MapControllers();
+
+app.Run();
